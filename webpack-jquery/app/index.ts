e576a1f98d09720aa588415a998325407f4c6db4
Nicolas Rousseau <Nicolas@MacBook-Pro-de-Nicolas-2.local>
2017-03-04 00:10:07 +0100

    Ajout exemple avec jquery

--- webpack-jquery/app/index.ts
+++ webpack-jquery/app/index.ts
@@ -1,3 +1,24 @@
-import {sayHello} from './component';
+import * as $  from "jquery";
 
-document.body.appendChild(sayHello("Nicolas"));
+$("body").append("Un test avec jQuery");
+
+interface ToDo {
+    label:string;
+}
+
+var todos:Array<ToDo>= [
+    {label:"Task 1"},
+    {label:"Task 2"},
+    {label:"Task 3"},
+    {label:"Task 4"},
+]
+
+$("body").append("<ul>");
+$.each(todos, (index, item:ToDo)=> $("ul").append(`<li>${item.label}</li>`))
+
+
+
+
+
+
+//document.body.appendChild(sayHello("Nicolas"));
