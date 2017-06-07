9e7c0905ca06e1aae54b35e28bda569e174444b4
Nicolas Rousseau <Nicolas@MacBook-Pro-de-Nicolas-2.local>
2017-06-07 22:06:48 +0200

    ajout lodash

--- 4-webpack-jquery/app/index.ts
+++ 4-webpack-jquery/app/index.ts
@@ -1,15 +1,31 @@
 import * as $  from "jquery";
 import {sayHello} from "./component"
+import * as _ from "lodash"
 
 $("#content").append(sayHello("Nicolas"));
 
 $("#content").append("Un test avec jQuery");
 
-var todos  = [
-    {titre:"Preparer le midi technique TypeScript"},
-    {titre:"Traiter les demandes PIC", description:"Description de la tache 2"},
-    {titre:"Preparer les vacances"},    
+enum State{A_FAIRE, TERMINER}
+
+interface Todo {
+    titre:string,
+    description?:string,
+    etat: State
+}
+
+var todos : Todo[] = [
+    {titre:"Preparer le midi technique TypeScript", etat:State.TERMINER},
+    {titre:"Traiter les demandes PIC", description:"Description de la tache 2", etat:State.A_FAIRE},
+    {titre:"Preparer les vacances", etat:State.A_FAIRE},    
 ]
 
 var ul = $("#content").append($("<ul>"));
 $.each(todos, (index, item)=> ul.append(`<li>${item.titre}</li>`))
+
+
+_.each(todos)
+    .filter(todo=> {console.log(todo.etat);return todo.etat === State.A_FAIRE})
+    .map(todo => {console.log(todo.titre); return _.toUpper(todo.titre);})
+    .forEach(titre => console.log(titre));
+
