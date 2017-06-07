f40ed697b3f613628dd0f6de41532dcc9549b03a
Nicolas Rousseau <Nicolas@MacBook-Pro-de-Nicolas-2.local>
2017-06-07 17:42:31 +0200

    Amelioration graphique

--- 1-Tester/todo.ts
+++ 1-Tester/todo.ts
@@ -1,20 +1,21 @@
+enum State{A_FAIRE, TERMINER}
 
 interface Todo {
     titre:string,
     description?:string,
-    etat?: string
+    etat: State
 }
 
 let todos : Todo[] = [
-    {titre:"Tache 1"},
-    {titre:"Tache 2", description:"Description de la tache 2"},
-    {titre:"Tache 3"},    
+    {titre:"Preparer le midi technique TypeScript", etat:State.TERMINER},
+    {titre:"Traiter les demandes PIC", description:"Description de la tache 2", etat:State.TERMINER},
+    {titre:"Preparer les vacances", etat:State.TERMINER},    
 ]
 
 console.log(todos.length);
 todos.forEach(todo => {
-    const div = document.createElement("div")
-    div.textContent = todo.titre
-    document.querySelector("#todo").appendChild(div);
+    const li = document.createElement("li")
+    li.innerHTML = `${todo.titre} Status : ${todo.etat === State.TERMINER? 'Realiser':'A faire'}`
+    document.querySelector("#todo").appendChild(li);
 }  
 )
