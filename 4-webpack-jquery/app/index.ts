1dee26538bd98477b54881216edfa5ee11271079
Nicolas Rousseau <nicolas.rousseau1@gmail.com>
2017-06-08 06:23:45 +0200

    Mise à jour du scenario

--- 4-webpack-jquery/app/index.ts
+++ 4-webpack-jquery/app/index.ts
@@ -1,15 +1,26 @@
+
 //realisation des imports
 
 
 
 //appel de la méthode sayHello
 
-var todos  = [
-    {titre:"Preparer le midi technique TypeScript"},
-    {titre:"Traiter les demandes PIC", description:"Description de la tache 2"},
-    {titre:"Preparer les vacances"},    
+var todos = [
+    { titre: "Preparer le midi technique TypeScript" },
+    { titre: "Traiter les demandes PIC", description: "Description de la tache 2" },
+    { titre: "Preparer les vacances" },
 ]
 
 // parcours de la liste
 var ul = $("#content").append($("<ul>"));
-$.each(todos, (index, item)=> ul.append(`<li>${item.titre}</li>`))
+$.each(todos, (index, item) => ul.append(`<li>${item.titre}</li>`))
+
+
+function searchWeather(town: string) {
+
+}
+
+//lors de la sortie, recherche de la meteo
+
+//apres chargement du document recherche Lille
+
